ab27c37ad32ace99bd466f6e006f68706c8a9270
arloor <admin@arloor.com>
2020-02-20 16:53:40 +0800

    nat <config>

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,7 @@
 use std::fs;
 use std::fs::File;
 use crate::IP::{remote_ip, local_ip};
+use std::process::exit;
 
 #[derive(Debug)]
 pub enum nat_cell {
@@ -22,10 +23,38 @@ impl nat_cell {
     pub fn build(&self) -> String {
         match &self {
             nat_cell::RANGE { port_start, port_end, remote_domain } =>
-                build_range(port_start, port_end, remote_domain)
-            ,
+                {
+                    let string = format!("#{cell:?}\n\
+                    add rule ip nat PREROUTING tcp dport {portStart}-{portEnd} counter dnat to {remoteIP}:{portStart}-{portEnd}\n\
+    add rule ip nat PREROUTING udp dport {portStart}-{portEnd} counter dnat to {remoteIP}:{portStart}-{portEnd}\n\
+    add rule ip nat POSTROUTING ip daddr {remoteIP} tcp dport {portStart}-{portEnd} counter snat to {localIP}\n\
+    add rule ip nat POSTROUTING ip daddr {remoteIP} udp dport {portStart}-{portEnd} counter snat to {localIP}\n\n\
+    ", cell = self, portStart = port_start, portEnd = port_end, remoteIP = match remote_ip(remote_domain) {
+                        Some(s) => s,
+                        None => return "".to_string(),
+                    }, localIP = match local_ip() {
+                        Some(s) => s,
+                        None => return "".to_string(),
+                    });
+
+                    string
+                }
             nat_cell::SINGLE { local_port, remote_port, remote_domain } =>
-                build_single(local_port, remote_port, remote_domain)
+                {
+                    let string = format!("#{cell:?}\n\
+                    add rule ip nat PREROUTING tcp dport {localPort} counter dnat to {remoteIP}:{remotePort}\n\
+                    add rule ip nat PREROUTING udp dport {localPort} counter dnat to {remoteIP}:{remotePort}\n\
+                    add rule ip nat POSTROUTING ip daddr {remoteIP} tcp dport {remotePort} counter snat to {localIP}\n\
+                    add rule ip nat POSTROUTING ip daddr {remoteIP} udp dport {remotePort} counter snat to {localIP}\n\n\
+                    ", cell = self, localPort = local_port, remotePort = remote_port, remoteIP = match remote_ip(remote_domain) {
+                        Some(s) => s,
+                        None => return "".to_string(),
+                    }, localIP = match local_ip() {
+                        Some(s) => s,
+                        None => return "".to_string(),
+                    });
+                    string
+                }
         }
     }
 
@@ -46,45 +75,22 @@ impl nat_cell {
     }
 }
 
-fn build_single(local_port: &i32, remote_port: &i32, remote_domain: &String) -> String {
-    let string = format!("add rule ip nat PREROUTING tcp dport {localPort} counter dnat to {remoteIP}:{remotePort}\n\
-    add rule ip nat PREROUTING udp dport {localPort} counter dnat to {remoteIP}:{remotePort}\n\
-    add rule ip nat POSTROUTING ip daddr {remoteIP} tcp dport {remotePort} counter snat to {localIP}\n\
-    add rule ip nat POSTROUTING ip daddr {remoteIP} udp dport {remotePort} counter snat to {localIP}\n\
-    ", localPort = local_port, remotePort = remote_port, remoteIP = match remote_ip(remote_domain) {
-        Some(s) => s,
-        None => return "".to_string(),
-    }, localIP = match local_ip() {
-        Some(s) => s,
-        None => return "".to_string(),
-    });
-    string
+
+pub fn example(conf: &String) {
+    println!("请在 {} 编写转发规则，内容类似：", &conf);
+    println!("{}", "SINGLE,10000,443,baidu.com\n\
+                    RANGE,1000,2000,baidu.com")
 }
 
-
-fn build_range(port_start: &i32, port_end: &i32, remote_domain: &String) -> String {
-    let string = format!("add rule ip nat PREROUTING tcp dport {portStart}-{portEnd} counter dnat to {remoteIP}:{portStart}-{portEnd}\n\
-    add rule ip nat PREROUTING udp dport {portStart}-{portEnd} counter dnat to {remoteIP}:{portStart}-{portEnd}\n\
-    add rule ip nat POSTROUTING ip daddr {remoteIP} tcp dport {portStart}-{portEnd} counter snat to {localIP}\n\
-    add rule ip nat POSTROUTING ip daddr {remoteIP} udp dport {portStart}-{portEnd} counter snat to {localIP}\n\
-    ", portStart = port_start, portEnd = port_end, remoteIP = match remote_ip(remote_domain) {
-        Some(s) => s,
-        None => return "".to_string(),
-    }, localIP = match local_ip() {
-        Some(s) => s,
-        None => return "".to_string(),
-    });
-
-    string
-}
-
-
-pub fn read_config() -> Vec<nat_cell> {
+pub fn read_config(conf: String) -> Vec<nat_cell> {
     let mut nat_cells = vec![];
-
-
-    let mut contents = fs::read_to_string("nat.config")
-        .expect("Something went wrong reading the file");
+    let mut contents = match fs::read_to_string(&conf) {
+        Ok(s) => s,
+        Err(e) => {
+            example(&conf);
+            exit(1);
+        }
+    };
     contents = contents.replace("\r\n", "\n");
 
     let strs = contents.split("\n");
--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,54 @@ use std::process::{exit, Command};
 use crate::IP::remote_ip;
 use std::fs::File;
 use std::io::Write;
+use std::{io, env};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut conf = String::new();
+    if args.len() != 2 {
+        let conf = "nat.conf".to_string();
+        println!("{}{}", "使用方式：nat ", conf);
+        config::example(&conf);
+        return;
+    } else {
+        conf += &args[1];
+    }
+
     //脚本的前缀
-    let script_prefix=String::from("#!/usr/sbin/nft -f\n\
+    let script_prefix = String::from("#!/usr/sbin/nft -f\n\
     \n\
     flush ruleset\n\
     add table ip nat\n\
     add chain nat PREROUTING { type nat hook prerouting priority -100 ; }\n\
-    add chain nat POSTROUTING { type nat hook postrouting priority 100 ; }\n");
+    add chain nat POSTROUTING { type nat hook postrouting priority 100 ; }\n\n");
 
-    let vec = config::read_config();
+    let vec = config::read_config(conf);
     let mut script = String::new();
     script += &script_prefix;
 
     for x in vec.iter() {
         let (domain, ip) = x.get_target_ip();
-        println!("{}-{}", domain, ip);
+//        println!("{}-{}", domain, ip);
         let string = x.build();
         script += &string;
 //        println!("{}",string)
     }
     println!("{}", script);
 
-    let mut f=File::create("temp.nft");
-    if let Ok(mut file) = f{
-        {println!("{:?}",file)}
+    let mut f = File::create("temp.nft");
+    if let Ok(mut file) = f {
         file.write_all(script.as_bytes()).expect("写失败");
     }
-    let output = Command::new("/usr/sbin/nft")
-        .arg("-f")
-        .arg("temp.nft")
-        .output()
-        .unwrap_or_else(|e| panic!("wg panic because:{}", e));
-    println!("output:");
-    let st = String::from_utf8_lossy(&output.stdout);
-    let lines = st.split("\n");
-    for line in lines {
-        println!("{}", line);
+
+    if cfg!(target_os = "linux") {
+        let output = Command::new("/usr/sbin/nft")
+            .arg("-f")
+            .arg("temp.nft")
+            .output()
+            .unwrap_or_else(|e| panic!("wg panic because:{}", e));
+        println!("status: {}", output.status);
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
     }
 }
