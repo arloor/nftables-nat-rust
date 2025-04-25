46bfae8bd8cf4bb43f75524441c6aacc91649755
arloor <admin@arloor.com>
2025-04-25 20:43:14 +0800

    test: 添加对默认源IP和远程IP解析的单元测试

--- src/ip.rs
+++ src/ip.rs
@@ -23,3 +23,34 @@ pub fn remote_ip(domain: &String) -> io::Result<String> {
         .map(|addr| addr.ip().to_string())
         .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Failed to resolve IPv4 address"))
 }
+
+#[allow(clippy::unwrap_used)]
+mod test {
+
+    #[test]
+    fn test_default_src_ip() {
+        use std::net::Ipv4Addr;
+        let ip = super::default_src_ip().unwrap();
+        println!("Default source IP: {}", ip);
+        assert!(!ip.is_empty());
+        assert!(ip.parse::<Ipv4Addr>().is_ok());
+    }
+
+    #[test]
+    fn test_remote_ip() {
+        use std::net::Ipv4Addr;
+        let domain = "www.google.com".to_string();
+        let ip = super::remote_ip(&domain).unwrap();
+        println!("Resolved IP for {}: {}", domain, ip);
+        assert!(!ip.is_empty());
+        assert!(ip.parse::<Ipv4Addr>().is_ok());
+    }
+
+    #[test]
+    fn test_remote_ip2() {
+        let domain = "example.asddddddddddddddddddddaasdasdasdasdasdasadasads.com".to_string();
+        let res = super::remote_ip(&domain);
+        println!("Resolved IP for {}: {:?}", domain, res);
+        assert!(res.is_err());
+    }
+}
